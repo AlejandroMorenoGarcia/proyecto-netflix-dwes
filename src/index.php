0204d1e6db4bb5c8d5ac7500126b6307cd2fe43f
Alejandro Moreno <alemorgar7@alu.edu.gva.es>
2026-09-24 20:30:26 +0200

    24-9-26 Clase

--- src/index.php
+++ src/index.php
@@ -1,3 +1,23 @@
 <?php
-include "app/Views/backend/index.html"
+    include_once "./vendor/autoload.php";
+
+    use Phroute\Phroute\Exception\HttpRouteNotFoundException;
+    use Phroute\Phroute\RouteCollector;
+
+    $router = new RouteCollector();
+
+    $router->get("/admin", function(){
+        return "Estas intentando acceder a la funcion admin";
+    });
+
+    $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
+    try{
+        $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    }catch (\Phroute\Phroute\Exception\HttpRouteNotFoundException $e){
+        $response = "Eres muy tonto";
+    }
+
+    echo $response;
+
+    // include "app/Views/backend/index.html";
 ?>
